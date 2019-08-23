00ef2f72ebcb97c46c2cfc1539d36eb66682960d
artyom <worldbeater-dev@yandex.ru>
2019-08-23 17:15:54 +0300

    Ability to download complex file trees

--- Rapi.Tests/RapiSftpTests.cs
+++ Rapi.Tests/RapiSftpTests.cs
@@ -20,12 +20,12 @@ namespace Rapi.Tests
         [Fact]
         public async Task ShouldDownloadFilesViaSftp()
         {
-            var connection = await RapiConnection.Connect(new HttpClientTransport(_host.Address));
-            var input = connection.Path.Combine(connection.FileSystemInfo.TempDirectory, "should_download_sftp_in");
+            var (connection, root) = await Connect();
+            var input = connection.Path.Combine(root, "should_download_sftp_in");
             await connection.FileSystem.WriteFileContents(input, Encoding.UTF8.GetBytes("super secret"));
             _output.WriteLine("should_download_sftp_in contents successfully written.");
 
-            var output = connection.Path.Combine(connection.FileSystemInfo.TempDirectory, "should_download_sftp_out");
+            var output = connection.Path.Combine(root, "should_download_sftp_out");
             _output.WriteLine($"Downloading file from {input} to {output}");
             await connection.Sftp.Download(input, output, _host.Configuration.Sftp);
 
@@ -37,18 +37,93 @@ namespace Rapi.Tests
         [Fact]
         public async Task ShouldUploadFilesViaSftp()
         {
-            var connection = await RapiConnection.Connect(new HttpClientTransport(_host.Address));
-            var input = connection.Path.Combine(connection.FileSystemInfo.TempDirectory, "should_upload_sftp_in");
+            var (connection, root) = await Connect();
+            var input = connection.Path.Combine(root, "should_upload_sftp_in");
             await connection.FileSystem.WriteFileContents(input, Encoding.UTF8.GetBytes("super secret"));
             _output.WriteLine("should_upload_sftp_in contents successfully written.");
 
-            var output = connection.Path.Combine(connection.FileSystemInfo.TempDirectory, "should_upload_sftp_out");
+            var output = connection.Path.Combine(root, "should_upload_sftp_out");
             _output.WriteLine($"Uploading file from {input} to {output}");
             await connection.Sftp.Upload(input, output, _host.Configuration.Sftp);
 
             var downloaded = await connection.FileSystem.ReadFileContents(output);
             var words = Encoding.UTF8.GetString(downloaded);
             Assert.Equal("super secret", words);
+        }
+        
+        [Fact]
+        public async Task ShouldDownloadDirectoriesViaSftp()
+        {
+            var (rapi, root) = await Connect();
+            var input = rapi.Path.Combine(root, "should_download_dir_in");
+            var sample = Encoding.UTF8.GetBytes("42");
+            
+            /**
+             * Here we build the following file tree:
+             *
+             * should_download_dir_in/
+             * |-- sample_1
+             * |   sample_2
+             * |   sample_dir/
+             * |   |-- sample_3
+             * |   +-- sample_4
+             * +-- sample_empty_dir/
+             */
+            
+            _output.WriteLine("Building file tree...");
+            await rapi.FileSystem.CreateDirectory(input);
+            await rapi.FileSystem.CreateDirectory(rapi.Path.Combine(input, "sample_dir"));
+            await rapi.FileSystem.CreateDirectory(rapi.Path.Combine(input, "sample_dir", "sample_nested_dir"));
+            await rapi.FileSystem.CreateDirectory(rapi.Path.Combine(input, "sample_empty_dir"));
+            foreach (var file in new[]
+            {
+                rapi.Path.Combine(input, "sample_1"),
+                rapi.Path.Combine(input, "sample_2"),
+                rapi.Path.Combine(input, "sample_dir", "sample_3"),
+                rapi.Path.Combine(input, "sample_dir", "sample_4"),
+                rapi.Path.Combine(input, "sample_dir", "sample_nested_dir", "sample_5")
+            })
+                await rapi.FileSystem.WriteFileContents(file, sample);
+
+            _output.WriteLine("Downloading files...");
+            var output = rapi.Path.Combine(root, "should_download_dir_out");
+            await rapi.FileSystem.CreateDirectory(output);
+            await rapi.Sftp.Download(input, output, _host.Configuration.Sftp);
+
+            _output.WriteLine("Asserting all files, folders and contents are correct...");
+            Assert.True(await rapi.FileSystem.DirectoryExists(rapi.Path.Combine(output, "sample_dir")));
+            Assert.True(await rapi.FileSystem.DirectoryExists(rapi.Path.Combine(output, "sample_empty_dir")));
+            foreach (var file in new[]
+            {
+                rapi.Path.Combine(output, "sample_1"),
+                rapi.Path.Combine(output, "sample_2"),
+                rapi.Path.Combine(output, "sample_dir", "sample_3"),
+                rapi.Path.Combine(output, "sample_dir", "sample_4"),
+                rapi.Path.Combine(output, "sample_dir", "sample_nested_dir", "sample_5")
+            })
+            {
+                await rapi.FileSystem.FileExists(file);
+                var bytes = await rapi.FileSystem.ReadFileContents(file);
+                var words = Encoding.UTF8.GetString(bytes);
+                Assert.Equal("42", words);
+            }
+            
+            _output.WriteLine("Asserting file and folder count...");
+            var files = await rapi.FileSystem.GetFiles(output);
+            var directories = await rapi.FileSystem.GetDirectories(output);
+            Assert.Equal(2, files.Count);
+            Assert.Equal(2, directories.Count);
+        }
+        
+        private async Task<(RapiConnection Connection, string Root)> Connect()
+        {
+            var connection = await RapiConnection.Connect(new HttpClientTransport(_host.Address));
+            var root = connection.Path.Combine(connection.FileSystemInfo.TempDirectory, _host.DirectoryName);
+            
+            if (!await connection.FileSystem.DirectoryExists(root))
+                await connection.FileSystem.CreateDirectory(root);
+            
+            return (connection, root);
         }
     }
 }
--- RapiAgent/Rpc/RapiSftpRpc.cs
+++ RapiAgent/Rpc/RapiSftpRpc.cs
@@ -10,22 +10,70 @@ namespace RapiAgent.Rpc
     {
         public async Task Download(string from, string to, RapiSftpCredentials credentials)
         {
+            from = GetSshNetFriendlyPath(from);
             using (var sftp = new SftpClient(credentials.Host, credentials.Port, credentials.Login, credentials.Password))
             {
                 sftp.HostKeyReceived += (sender, args) => args.CanTrust = true;
                 sftp.Connect();
                 try
                 {
-                    using (var stream = File.Create(to))
-                    {
-                        stream.Seek(0, SeekOrigin.Begin);
-                        var path = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-                            ? $"/{from.Replace(@"\", "/")}"
-                            : from;
+                    var remoteEntry = sftp.Get(from);
+                    if (remoteEntry.IsDirectory)
+                        await DownloadDirectory(sftp, from, to);
+                    else if (remoteEntry.IsRegularFile)
+                        await DownloadFile(sftp, from, to);
+                }
+                finally
+                {
+                    sftp.Disconnect();
+                }
+            }
+        }
+        
+        private static async Task DownloadDirectory(SftpClient sftp, string from, string to)
+        {
+            var remoteFiles = sftp.ListDirectory(from);
+            foreach (var remoteFile in remoteFiles)
+            {
+                if (remoteFile.IsSymbolicLink || 
+                    remoteFile.Name == "."    || 
+                    remoteFile.Name == "..") 
+                    continue;
 
-                        sftp.DownloadFile(path, stream);
-                        await stream.FlushAsync();
-                    }
+                if (remoteFile.IsDirectory)
+                {
+                    var localDirectoryPath = Path.Combine(to, remoteFile.Name);
+                    var localDirectory = Directory.CreateDirectory(localDirectoryPath);
+                    await DownloadDirectory(sftp, remoteFile.FullName, localDirectory.FullName);
+                }
+                else if (remoteFile.IsRegularFile)
+                {
+                    var localFilePath = Path.Combine(to, remoteFile.Name);
+                    await DownloadFile(sftp, remoteFile.FullName, localFilePath);
+                }
+            }
+        }
+
+        private static Task DownloadFile(SftpClient sftp, string from, string to) => Task.Run(() =>
+        {
+            using (var stream = File.Create(to))
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                sftp.DownloadFile(from, stream);
+                stream.Flush();
+            }
+        });
+
+        public async Task Upload(string from, string to, RapiSftpCredentials credentials)
+        {
+            using (var sftp = new SftpClient(credentials.Host, credentials.Port, credentials.Login, credentials.Password))
+            {
+                sftp.HostKeyReceived += (sender, args) => args.CanTrust = true;
+                sftp.Connect();
+                try
+                {
+                    to = GetSshNetFriendlyPath(to);
+                    await UploadFile(sftp, from, to);
                 }
                 finally
                 {
@@ -34,30 +82,20 @@ namespace RapiAgent.Rpc
             }
         }
 
-        public Task Upload(string from, string to, RapiSftpCredentials credentials)
+        private static Task UploadFile(SftpClient sftp, string from, string to) => Task.Run(() =>
         {
-            using (var sftp = new SftpClient(credentials.Host, credentials.Port, credentials.Login, credentials.Password))
+            using (var stream = File.OpenRead(from))
             {
-                sftp.HostKeyReceived += (sender, args) => args.CanTrust = true;
-                sftp.Connect();
-                try
-                {
-                    using (var stream = File.OpenRead(from))
-                    {
-                        stream.Seek(0, SeekOrigin.Begin);
-                        var path = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-                            ? $"/{to.Replace(@"\", "/")}"
-                            : to;
-
-                        sftp.UploadFile(stream, path);
-                    }
-                }
-                finally
-                {
-                    sftp.Disconnect();
-                }
+                stream.Seek(0, SeekOrigin.Begin);
+                sftp.UploadFile(stream, to);
             }
-            return Task.CompletedTask;
+        });
+
+        private static string GetSshNetFriendlyPath(string path)
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? $"/{path.Replace(@"\", "/")}"
+                : path;
         }
     }
 }
